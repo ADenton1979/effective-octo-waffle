772a0101b4c9980ff3d5232f26537f2f746e32e5
Adam Denton <adenton@localhost.localdomain>
2019-04-28 21:43:42 -0600

    I made changes

--- src/newpythonproject.py
+++ src/newpythonproject.py
@@ -5,7 +5,14 @@
 __author__ = "adenton"
 __date__ = "$Apr 28, 2019 9:24:00 PM$"
 
+def print_hi():
+    print("hi")
+
+def other_function():
+    print("I do other things")
+
 if __name__ == "__main__":
-    while True:
-        print "Hello World"
-        break
+    for x in range(0,10):
+        print_hi()
+    other_function()
+    
