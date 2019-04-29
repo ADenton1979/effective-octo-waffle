5dc53148744604fb26da54c49b050e2163618a96
Adam Denton <adenton@localhost.localdomain>
2019-04-28 21:39:30 -0600

    Initial Commit

---
+++ src/newpythonproject.py
@@ -0,0 +1,11 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "adenton"
+__date__ = "$Apr 28, 2019 9:24:00 PM$"
+
+if __name__ == "__main__":
+    while True:
+        print "Hello World"
+        break
